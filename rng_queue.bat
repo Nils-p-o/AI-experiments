5bba653508ea23c1d8f1dc542986ce4d6f70b4ec
Nils-p-o <nilspauls28@gmail.com>
2025-07-25 06:50:58 +0300

    rng_queue

--- rng_queue.bat
+++ rng_queue.bat
@@ -5,13 +5,13 @@
 
 
 @REM C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2.py --config "./experiment_configs/triplicate.json"
-@REM C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2.py --config "./experiment_configs/triplicate.json"
-@REM C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2.py --config "./experiment_configs/triplicate.json"
-C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2_MTP.py --config "./experiment_configs/MTP_triplicate.json"
-C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2_MTP.py --config "./experiment_configs/MTP_triplicate.json"
 @REM C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2_MTP.py --config "./experiment_configs/MTP_triplicate.json"
+@REM C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2_MTP_exp.py --config "./experiment_configs/MTP_experiment_trip.json"
+C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2_MTP_exp.py --config "./experiment_configs/MTP_classification_exp.json"
+@REM C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2_MTP_exp.py --config "./experiment_configs/MTP_experiment_trip.json"
+@REM C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe c:/Users/nilsp/Documents/AI-experiments/money_train_2_MTP_exp.py --config "./experiment_configs/MTP_experiment_trip.json"
 
-C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe C:\Users\nilsp\OneDrive\Dokumenti\AI-experiments\money_train_2_MTP.py --config "./experiment_configs/MTP_triplicate.json"
+@REM C:/Users/nilsp/AppData/Local/Programs/Python/Python311/python.exe C:\Users\nilsp\OneDrive\Dokumenti\AI-experiments\money_train_2_MTP.py --config "./experiment_configs/MTP_triplicate.json"
 
 
 
